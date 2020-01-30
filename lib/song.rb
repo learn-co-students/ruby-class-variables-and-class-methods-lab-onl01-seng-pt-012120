c023dba7e714f04cdbf249d6e376307e20fed2f2
Adam G Horn <hornadam8@gmail.com>
2020-01-30 17:53:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(song,artist,genre)
+    @@count += 1
+    @name = song
+    @genre = genre
+    @@genres << @genre
+    @artist = artist
+    @@artists << @artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    count = {}
+    @@genres.each{|genre| count.include?(genre) ? count[genre] += 1 : count[genre] = 1}
+    count
+  end
+  
+  def self.artist_count
+    count = {}
+    @@artists.each{|artist| count.include?(artist) ? count[artist] += 1 : count[artist] = 1}
+    count
+  end
+  
+end
+    
