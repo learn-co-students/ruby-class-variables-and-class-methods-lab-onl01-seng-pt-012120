1f6d0a5a1e48068714a0bd70fd0fcd5f24b89e09
Patricia Cordero <patricia@patriciaco.com>
2020-03-12 08:52:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,80 @@
+
+  
+
+  class Song
+     attr_accessor :name, :artist, :genre
+   
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    
+  
+    
+  def initialize(song_name, artist, genre)
+      @name = song_name
+      @artist = artist
+      @genre = genre
+      @@count += 1
+      # @@genres.push--f-f("genre")
+       @@genres << genre
+       @@artists << artist
+      # @@artists.push("artist" )
+  end
+  
+  def self.count
+      @@count
+  end
+     
+    # @@name = []
+    # @@artist = []
+    # @@genres = []
+ 
+  
+  def self.artists
+      @@artists.uniq
+  end
+
+    
+     
+  def self.genres
+    @@genres.uniq
+  end
+
+
+    
+  def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+    if genre_count[genre] 
+        genre_count[genre] += 1
+    else 
+        genre_count[genre] = 1
+    end
+  end
+    genre_count
+ end
+
+    
+  def self.artist_count
+      artist_count = {}
+      @@artists.each do |artist|
+    if artist_count[artist]
+          artist_count[artist] += 1
+    else
+        artist_count[artist] = 1
+      end
+    end
+      artist_count
+    end
+  end
+    
+    
+#       def count 
+#         each.count_with_index |num, i|
+#         num + i 
+#   end
+# end
+    
+    
+  
